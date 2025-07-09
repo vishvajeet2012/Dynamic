84b1c812902fa1ce7ef64aee737defe482936bcb
vishvajeet shukla <djritikshukla@gmail.com>
2025-07-09 19:53:01 +0530

    updaye

--- server/models/authModel.js
+++ server/models/authModel.js
@@ -7,21 +7,21 @@ const jwt = require('jsonwebtoken');
 const userSchema = new Schema({
     firstName: {
         type: String,
-        required: [true, 'First name is required']
+        
     },
     lastName: {
         type: String,
-        required: [true, 'Last name is required']
+   
     },
     email: {
         type: String,
-        required: [true, 'Email is required'],
+       
         unique: true,
         lowercase: true
     },
     password: {
         type: String,
-        required: [true, 'Password is required'],
+       
         minlength: [6, 'Password must be at least 6 characters']
     },
     isAdmin: {
