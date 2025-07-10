d24b8205f7a4bd0065ac5fad3bdcebc8fcd7d1fd
vishvajeet shukla <djritikshukla@gmail.com>
2025-07-10 11:26:35 +0530

    multer storege remove

--- server/middleware/uploadImageMiddleware.js
+++ server/middleware/uploadImageMiddleware.js
@@ -2,18 +2,18 @@ const multer = require("multer");
 const path = require("path");
 
 //set our multer storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(
+//       null,
 
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
+//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+//     );
+//   },
+// });
 
 //file filter function
 const checkFileFilter = (req, file, cb) => {
@@ -26,7 +26,7 @@ const checkFileFilter = (req, file, cb) => {
 
 //multer middleware
 module.exports = multer({
-  storage: storage,
+ 
   fileFilter: checkFileFilter,
   limits: {
     fileSize: 5 * 1024 * 1024, //5MB file size limit
