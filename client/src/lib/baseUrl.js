b50e76da64c6aa60183b17de64298e7e2a86901d
vishvajeet shukla <djritikshukla@gmail.com>
2025-07-10 20:11:35 +0530

    vi

--- client/src/lib/baseUrl.js
+++ client/src/lib/baseUrl.js
@@ -1,9 +1,9 @@
-// export const homeUrl = 'https://vstoreserver.vercel.app/home';
-// export const baseUrl='https://vstoreserver.vercel.app/api'
+export const homeUrl = 'https://vstoreserver.vercel.app/home';
+export const baseUrl='https://vstoreserver.vercel.app/api'
 
 
-export const homeUrl = 'http://localhost:4004/home';
-export const baseUrl='http://localhost:4004/api'
+// export const homeUrl = 'http://localhost:4004/home';
+// export const baseUrl='http://localhost:4004/api'
 
 
 //https://vstoreserver.vercel.app/
