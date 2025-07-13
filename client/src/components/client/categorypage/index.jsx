132fb5594a9f87222ac9e0bc904f99fbc41cb64b
vishvajeet shukla <djritikshukla@gmail.com>
2025-07-13 18:25:27 +0530

    udas

--- client/src/components/client/categorypage/index.jsx
+++ client/src/components/client/categorypage/index.jsx
@@ -3,14 +3,14 @@ import { useProductByKeys } from "../../../hooks/Product/Product";
 import { useEffect, useState } from "react";
 
 export default function CategroyPage() {
-const [categoryIds, setCategory] = useState(null);
+const [categoryIds, setCategory] = useState([]);
 const [keyword, setKeyword] = useState(null);
 const {id} = useParams()
 console.log(id); 
 
   const {ProductByKeys   ,loading ,Product, error,success}= useProductByKeys()
   useEffect(() => {
-      setCategory(id)
+      setCategory([id])
       
       
       
