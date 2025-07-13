f0da25fcdc59663152d3aa2dae11bc09c8557e7d
vishvajeet shukla <djritikshukla@gmail.com>
2025-07-13 17:23:06 +0530

    update

--- client/src/components/client/categorypage/index.jsx
+++ client/src/components/client/categorypage/index.jsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom";
 import { useProductByKeys } from "../../../hooks/Product/Product";
 
 export default function CategroyPage() {
 
-const params = useParams(); 
+const {id} = useParams()
+console.log(id); 
+
   const {ProductByKey   ,loading ,Product, error,success}= useProductByKeys()
   
     return (
