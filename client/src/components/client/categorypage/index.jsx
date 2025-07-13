4d7a2300d326179b798848e21abf6ab3d7648bc9
vishvajeet shukla <djritikshukla@gmail.com>
2025-07-13 17:40:40 +0530

    setup cateogry page

--- client/src/components/client/categorypage/index.jsx
+++ client/src/components/client/categorypage/index.jsx
@@ -1,13 +1,21 @@
 import { useParams } from "react-router-dom";
 import { useProductByKeys } from "../../../hooks/Product/Product";
+import { useEffect } from "react";
 
 export default function CategroyPage() {
-
+const [categoryIds, setCategory] = useState(null);
+const [keyword, setKeyword] = useState(null);
 const {id} = useParams()
 console.log(id); 
 
   const {ProductByKey   ,loading ,Product, error,success}= useProductByKeys()
-  
+  useEffect(() => {
+      setCategory(id)
+      if(categoryIds){
+        ProductByKey(keyword,categoryIds)
+      }
+  },[id])
+  console.log(Product)
     return (
         <div>
            
