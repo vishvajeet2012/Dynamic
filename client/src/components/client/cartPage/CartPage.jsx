223da1829a1454498248bdfcd1e791ca0b9b4480
vishvajeet <djritikshukla@gmai.com>
2025-07-27 18:44:49 +0530

    vis

--- client/src/components/client/cartPage/CartPage.jsx
+++ client/src/components/client/cartPage/CartPage.jsx
@@ -1,56 +1,80 @@
 import { useWishlist } from "../../../../context/wishListhContext";
 
-export default function CartPageProduct(){
-             const { toggleWishlist,wishlistItems, isInWishlist,productsWithWishlistStatus } = useWishlist();
+export default function CartPageProduct() {
+  const { toggleWishlist, wishlistItems, isInWishlist, productsWithWishlistStatus } = useWishlist();
 
 
-return (
-            <>
+  return (
+    <>
 
-                        <div className="border      h-screen">
-                <div className="border  border-black  "
->  
+      <div className="border      h-auo">
+        <div className=" border-black  "
+        >
 
-                                <div className=" px-4 ">
-                                        <p className="text-base font-bold">My Bag (0 item)</p>           
-                                </div>
-
-
-                        
-                         <div className=" ">
-                                  {productsWithWishlistStatus?.wishlist &&
-                                  productsWithWishlistStatus?.wishlist?.map((product, idx) => (console.log(product),
-                                      <div className="flex  gap-4 border-b border-gray-200 p-4"
-                                        key={product._id || idx}
-                                      >
-                                          <div className="" >
-                                                    <img className="h-52 object-contain" src={product?.images?.[0]?.imagesUrls} alt="product image" />
-
-                                        
-                                      
-</div>    <p className="font-bold ">{product?.name}</p>
-        <p  className="text-gray-300">Color:{product?.color}</p>
-                                      </div>
-                                    ))}
-                                </div>
-
-                
-
-
-</div>
-
-        
+          <div className=" border-b py-2 px-4 ">
+            <p className="text-base font-bold">My Bag (0 item)</p>
+          </div>
 
 
 
+          <div className=" border-b p-4">
+            {productsWithWishlistStatus?.wishlist &&
+              productsWithWishlistStatus?.wishlist?.map((product, idx) => (console.log(product),
+                <div className="flex   gap-4 border-b border-gray-200 p-4"
+                  key={product._id || idx}
+                >
+                  <div className="h-52 " >
+                    <img className="h-52 object-contain aspect-[4/5]" src={product?.images?.[0]?.imagesUrls} alt="product image" />
 
 
-                        </div>
+
+                  </div>
+                  <div className="flex flex-col md:flex-row justify-between w-full">
+                    <div className="flex flex-col gap-2">
+                      <p className="font-bold ">{product?.name}</p>
+                      <div className="flex flex-row items-center text-sm gap-2">
+                        <p className="font-bold">{product?.category?.categoryName} :</p>
+                        <p>{product?.subcategories?.[0]?.subCategoryName}</p></div>
+                      <p className="text-sm text-gray-400"><span className="text-black pr-1 font-bold text-sm">Color:</span>{product?.color}</p>
+                     {product?.theme&& <p className="text-sm text-gray-400"> <span className="text-black pr-1 font-bold text-sm">Color:</span> {product?.theme}</p>}
+                      <p className="text-sm text-gray-500">Quantity: <span className="text-black font-bold">{product?.quantity||"1"}</span></p>
+
+                    </div>
+                    <div classNa me="flex flex-row md:flex-col gap-2">
+                      <p className="text-base text-gray-500">Price: <span className="text-black font-bold">{product?.sellingPrice}</span></p>
+
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+            <div className="flex justify-between items-center border-b p-4">
+              <p className="text-sm text-gray-500">Total:</p>
+
+            </div>
+
+
+
+          </div>
 
 
 
 
-            </>
+        </div>
 
-   
-)}
+
+
+
+
+
+
+      </div>
+
+
+
+
+    </>
+
+
+  )
+}
