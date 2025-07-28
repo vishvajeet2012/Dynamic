dcf4ae71df1171bbbd491eb2256dc3db3972cdf2
vishvajeet <djritikshukla@gmai.com>
2025-07-28 19:37:26 +0530

    vish

--- server/controler/cart.js
+++ server/controler/cart.js
@@ -2,105 +2,65 @@ const Product =require('../models/ProductModel')
 const  User = require('../models/authModel')
 
 
-exports.addToCartControler = async (req,res)=>{
-
-    try {
+exports.addToCartControler = async (req, res) => {
+  try {
     const { productId, quantity, size, color } = req.body;
-             const userId = req.user._id;
+    const userId = req.user._id;
 
     if (!productId || !quantity || !size || !color) {
-        return res.status(400).json({ message: `All fields are required `  });
-    }
-   
-
-
-const  product = await Product.findById(productId);
-if(!product) {
-        return res.status(404).json({ message: 'Product not found' });
+      return res.status(400).json({ message: 'All fields are required' });
     }
 
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
 
-if(!product.size.includes(size)){
-            return res.status(400).json({message: `Size ${size} is not available for this product`});
-}
-                const user  = await User.findById(userId)
-                if(!user) return res.status(404).json({message:"user not found "})
+    if (!product.size.includes(size)) {
+      return res
+        .status(400)
+        .json({ message: `Size ${size} is not available for this product` });
+    }
 
-                          
-if (existingCartItem > -1) {
-  user.cart[existingCartItem].quantity += quantity;
-  await user.save();
-  
-  return res.status(200).json({
-    message: "Product already in cart, quantity updated",
-    cart: user.cart,
-    status: true,
-  });
-}
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: 'User not found' });
 
-user.cart.push({
-  product: productId,
-  quantity,
-  size,
-  color,
-});
+    const existingCartItem = user.cart.findIndex(
+      (item) =>
+        item.product.toString() === productId &&
+        item.size === size &&
+        item.color.toLowerCase() === color.toLowerCase()
+    );
 
-await user.save();
-return res.status(200).json({
-  message: "Product added to cart successfully",
-  cart: user.cart,
-  status: true,
-});
-}catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
-    }   
+    if (existingCartItem > -1) {
+      user.cart[existingCartItem].quantity += quantity;
+      await user.save();
 
+      return res.status(200).json({
+        message: 'Product already in cart, quantity updated',
+        cart: user.cart,
+        status: true,
+      });
+    }
 
+    user.cart.push({
+      product: productId,
+      quantity,
+      size,
+      color,
+    });
 
-}
+    await user.save();
 
-
-
-
-exports.getCartItems =async(req,res)=>{
-                try{
-                    const userId = req.user._id;
-                   const user = await User.findById(userId).populate('cart.product');
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-                            const cartItem =user.cart.map((item)=>{
-                                const product = item.product;
-                                const totalPrice = product.sellingPrice *item.quantity;
-                                return {
-                                    
-                                        id:product?._id,
-                                        name:product?.name,
-                                        images: product?.images,
-                                        color:item?.color,
-                                        size:item?.size,
-                                        sellingPrice:product?.sellingPrice,
-                                        theme: product?.theme,
-                                        quantity: item?.quantity,
-                                        totalPrice:totalPrice,
-
-                                
-                    
-                                }
-                            })
-const totalAmount =cartItem.reduece((sum,item)=>sum +item.totalPrice,0)
-
-
-                             
-
-
-res.status(200).json({cart:cartItem,totelAmount:totalAmount,status:true});
-
-
-                }catch(error){
-                    return res.status(500).json({ message: 'Internal server error' ,status:false});
-                }
-
-}
+    return res.status(200).json({
+      message: 'Product added to cart successfully',
+      cart: user.cart,
+      status: true,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error', status: false });
+  }
+};
 
 
 exports.updateCartItem = async (req, res) => {
