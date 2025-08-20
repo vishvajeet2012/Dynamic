de9b141225f522dff7a911e5c647d9137cddad7e
vishvajeet <djritikshukla@gmail.com>
2025-08-20 16:02:17 +0530

    vish

--- client/src/components/client/categorypage/CateogryProduct.jsx
+++ client/src/components/client/categorypage/CateogryProduct.jsx
@@ -282,7 +282,7 @@ const [pageSelect ,setPageSelect] = useState()
               </div>
 
               {Productloading ? (
-                <div className="flex justify-center items-center h-72">
+                <div className="flex justify-center items-center h-64">
                   <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#e11b23]"></div>
                 </div>
               ) : (
